31522c125e9b6c364eb88d22fae9f94b740a582e
Vitor <vi7org@hotmail.com>
2024-10-23 14:34:32 -0400

    implement fetching and writing of webpage

---
+++ fetcher.js
@@ -0,0 +1,42 @@
+﻿const url = process.argv[2];
+const filePath = process.argv[3];
+const fs = require('fs').promises;
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const needle = require('needle');
+
+const writePage = async function () {
+    try {
+        const fileExists = await fs.access(filePath).then(() => true).catch(() => false);
+        if (fileExists){
+            const answer = await new Promise((resolve) => {
+                rl.question(`The file ${filePath} already exists. Do you want to overwrite it? (y/n): `, resolve);
+            });
+            
+            if (answer !== 'y'){
+                console.log("Cancelling...");
+                rl.close();
+                process.exit(0);
+            }
+        }
+        const response = await needle('get', url);
+
+        if (response.statusCode !== 200) {
+            console.error(`Status code error: ${response.statusCode}`);
+        }
+
+        await fs.writeFile(filePath, response.body, 'utf8');
+        console.log(`File has been written successfully at ${filePath}`);
+        process.exit(0);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(-1);
+    }
+}
+
+writePage();
